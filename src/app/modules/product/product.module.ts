9ba369cd388d785ad38c2b170ce1601d92bbca1d
Erick García <erick_sonico@ciencias.unam.mx>
2023-10-06 20:57:50 -0600

    Imports in product.module

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './components/category/category.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
