92565694087bd488073221e76e0cbedf74ac5ccf
Jose Luis Lopez <joseluis27@ciencias.unam.mx>
2023-11-05 21:33:29 -0600

    modulo productos y shared

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { CategoryComponent } from './modules/product/components/category/category.component';
+import { ProductComponent } from './modules/productos/components/product/product/product.component';
 
 
 const routes: Routes = [
-  { path: "category", component: CategoryComponent }
+  { path: "category", component: CategoryComponent },
+  { path: "product", component: ProductComponent },
 ];
 
 @NgModule({
