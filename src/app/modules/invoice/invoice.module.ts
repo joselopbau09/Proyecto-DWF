4b3f174734f698f9014a5cff5980bfe6549303cb
eduardo-reyes <eduardoreyes@ciencias.unam.mx>
2023-11-17 11:27:39 -0600

    Componente invoice

--- src/app/modules/invoice/invoice.module.ts
+++ src/app/modules/invoice/invoice.module.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { InvoiceComponent } from './components/invoice/invoice.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InvoiceComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
