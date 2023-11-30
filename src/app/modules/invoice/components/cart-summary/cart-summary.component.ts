6a919f2c3e2be23d400e93df898ed9e6f48770bd
Jose Luis Lopez <joseluis27@ciencias.unam.mx>
2023-11-29 21:29:26 -0600

    Mostrar total de compra del carrito

--- src/app/modules/invoice/components/cart-summary/cart-summary.component.ts
+++ src/app/modules/invoice/components/cart-summary/cart-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 import { DtoCartDetails } from '../../_dtos/dto-cart-details';
 import { CartService } from '../../_services/cart.service';
@@ -8,18 +8,36 @@ import { CartService } from '../../_services/cart.service';
   templateUrl: './cart-summary.component.html',
   styleUrls: ['./cart-summary.component.css']
 })
-export class CartSummaryComponent {
+export class CartSummaryComponent implements OnChanges{
 
   public rfc: string = 'SAAI920101A01';
 
   public productosComprados: DtoCartDetails[] = [];
+
+  public total:number = 0;
+  
+  
   @Input()
   public productosCarrito!: DtoCartDetails[];
-
+  
   constructor(
     private cartService: CartService,
-  ) {}
+    ) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    this.calcularTotal();
+    for (let key in changes) {
+      console.log(`${key} changed.
+      Current: ${changes[key].currentValue}.
+      Previous: ${changes[key].previousValue}`);
+    }
+  }
   
+  public calcularTotal():void {
+    this.productosCarrito.forEach( producto => {
+      this.total += producto.product.price;
+    });
+  }
+
   public deleteCart():void {
     this.cartService.deleteCart(this.rfc).subscribe(
       res => {
@@ -27,6 +45,4 @@ export class CartSummaryComponent {
       }
     );
   }
-
-
 }
